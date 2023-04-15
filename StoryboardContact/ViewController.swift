89e613b41aae2ebf160f327bf6c1e6f46d089201
Bekhruzjon Hakmirzaev <hakmirzaevbekhruzjon@gmail.com>
2023-04-15 23:51:35 +0500

    Initial Commit

---
+++ StoryboardContact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardContact
+//
+//  Created by Bekhruz Hakmirzaev on 15/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
