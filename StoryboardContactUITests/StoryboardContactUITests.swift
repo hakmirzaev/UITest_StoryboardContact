ad6e424b69313d93204e7c8c389483d7b9443922
Bekhruzjon Hakmirzaev <hakmirzaevbekhruzjon@gmail.com>
2023-04-30 11:35:08 +0500

    UITest for StoryboardContact

--- StoryboardContactUITests/StoryboardContactUITests.swift
+++ StoryboardContactUITests/StoryboardContactUITests.swift
@@ -9,33 +9,40 @@ import XCTest
 
 final class StoryboardContactUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    var app: XCUIApplication!
+    
+    override  func setUp() {
+        super.setUp()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+    
+    func testWithNavigationTitle() throws{
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.navigationBars["Storyboard Contact"].exists)
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
+    
+    func testWithTableViewExist() throws{
+        app.launch()
+        
+        let ex = expectation(description: "testWithTableViewExist")
+        
+        AFHttp.get(url: AFHttp.API_CONTACT_LIST, params: AFHttp.paramsEmpty(), handler: { response in
+            switch response.result {
+            case .success:
+                XCTAssertTrue(self.app.tables.element.exists)
+                ex.fulfill()
+            case let .failure(error):
+                XCTAssertNil(error)
+                ex.fulfill()
+            }
+        })
+        
+        waitForExpectations(timeout: 10) { (error) in
+            if let error = error {
+                XCTFail("error: \(error)")
             }
         }
     }
+
 }
